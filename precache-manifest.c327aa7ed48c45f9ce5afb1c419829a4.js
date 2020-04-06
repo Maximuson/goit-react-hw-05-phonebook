15723ea83ec20ce52ec048e3c65faa12c7184afa
Maxim Pavlenko <maxim@MacBook-Air.local>
2020-04-06 17:06:36 +0300

    Updates

---
+++ precache-manifest.c327aa7ed48c45f9ce5afb1c419829a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9cad2a74da4e71d885edd587c049e0",
+    "url": "/goit-react-hw-5-phonebook/index.html"
+  },
+  {
+    "revision": "d0d329ddea06e73a35d1",
+    "url": "/goit-react-hw-5-phonebook/static/css/main.edfdf93e.chunk.css"
+  },
+  {
+    "revision": "b9590d58d9322241d26c",
+    "url": "/goit-react-hw-5-phonebook/static/js/2.78708ca2.chunk.js"
+  },
+  {
+    "revision": "1baa02233bbd827e3584e32091a75902",
+    "url": "/goit-react-hw-5-phonebook/static/js/2.78708ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d329ddea06e73a35d1",
+    "url": "/goit-react-hw-5-phonebook/static/js/main.66d7e48a.chunk.js"
+  },
+  {
+    "revision": "8e516615fd1a61dd62a9",
+    "url": "/goit-react-hw-5-phonebook/static/js/runtime-main.06496efd.js"
+  }
+]);
